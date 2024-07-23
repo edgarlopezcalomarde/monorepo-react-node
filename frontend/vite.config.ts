063ffc302b0390e5ebdf70b9c9ebf66f72969b6b
Edgar Lopez Calomarde <edgarlopezcalomarde@gmail.com>
2024-07-23 21:15:59 +0200

    vite config setup modified

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-
   server: {
+    host: true,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  preview: {
     host: true,
     proxy: {
       '/api': 'http://backend:3000',
