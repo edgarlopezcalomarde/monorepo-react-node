4b842a4507636eab85a3e81fc95d80f080878f7e
Edgar Lopez Calomarde <edgarlopezcalomarde@gmail.com>
2024-07-26 19:45:30 +0200

    db added

--- backend/src/product/ProductController.ts
+++ backend/src/product/ProductController.ts
@@ -6,6 +6,9 @@ export class ProductController {
     constructor(private getAllProducts: GetAllProducts) { }
 
     getAll = (req: Request, res: Response) => {
+        console.log("Holaaaa reiniciado1 55 ");
+
+        // console.log("Holaaaa reiniciado 2");
         res.json({
             data: this.getAllProducts.execute()
         })
